0802c838cf11951f45dd16efa25679004a1fc48b
HP@Jarvis <HP@Jarvis>
2021-08-30 10:29:38 +0530

    oag added

---
+++ oag/src/App.js
@@ -0,0 +1,103 @@
+import logo from './logo.svg';
+import './App.css';
+function App()
+{
+  return(
+<div className="container col-12">
+        <div className="container-fluid border-dark bg-dark">
+          <div className="container col-4">
+              <h1 className="col-10 text-left "className="text-light">About Us Page</h1>
+          </div>
+        </div>
+      <form className="form-horizontal">
+              <div className="text-center">
+                        <div className="text-center">
+                          <img src="..." className="rounded" alt="..."/>
+                        </div>
+                <div>
+                      <h2>Uddipta Goggoi</h2>
+                            <p className="title">Project Leader</p>
+                            <p>project leader of the team 26</p>
+                            <p>udipta@gmail.com</p>
+                            <p><button type="button" className="btn btn-outline-dark">Contact</button></p>
+               </div>
+              </div>
+              <div className="row">
+                    <div className="col">
+                    <div className="text-center">
+                                          <div className="text-center">
+                                            <img src="..." className="rounded" alt="..."/>
+                                          </div>
+                                  <div>
+                                        <h2>Vrushabh Chaudhari</h2>
+                                              <p className="title">Front End designer</p>
+                                              <p>knows the frontend languages</p>
+                                              <p>vrushabh@gmail.com</p>
+                                              <p><button type="button" className="btn btn-outline-dark">Contact</button></p>
+                                </div>
+                                </div>
+                    </div>
+                    <div className="col">
+                    <div className="text-center">
+                                          <div className="text-center">
+                                            <img src="..." className="rounded" alt="..."/>
+                                          </div>
+                                  <div>
+                                        <h2>Payal Arora</h2>
+                                        <p className="title">Front End designer</p>
+                                              <p>knows the frontend languages</p>
+                                              <p>payal@gmail.com</p>
+                                              <p><button type="button" className="btn btn-outline-dark">Contact</button></p>
+                                </div>
+                                </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                    <div className="text-center">
+                                          <div className="text-center">
+                                            <img src="..." className="rounded" alt="..."/>
+                                          </div>
+                                  <div>
+                                        <h2>Rohan DeshMukh</h2>
+                                              <p className="title">Back End Developer</p>
+                                              <p>Backend</p>
+                                              <p>Rohan@gmail.com</p>
+                                              <p><button type="button" className="btn btn-outline-dark">Contact</button></p>
+                                </div>
+                                </div>
+                    </div>
+                    <div className="col">
+                    <div className="text-center">
+                                          <div className="text-center">
+                                            <img src="..." className="rounded" alt="..."/>
+                                          </div>
+                                  <div>
+                                        <h2>Suraj Chavan</h2>
+                                        <p className="title">Back End Developer</p>
+                                              <p>Backend</p>
+                                              <p>Suraj@gmail.com</p>
+                                              <p><button type="button" className="btn btn-outline-dark">Contact</button></p>
+                                </div>
+                                </div>
+                    </div>
+                  </div>
+      </form>
+      <div className="card text-center">
+  <div className="card-header">
+    Featured
+  </div>
+  <div className="card-body" className="container-fluid border-dark bg-dark">
+    <h5 className="card-title" className="text-light" >Contact us</h5>
+    <p className="card-text" className="text-light">Online Art Gallety</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+  <div className="card-footer text-muted">
+    @gmail.com
+  </div>
+</div>
+</div>
+    
+  );
+}
+export default App;
